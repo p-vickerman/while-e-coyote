ddba708dd9beb053ff4517b14d799543c286888e
p-vickerman <patrick@vickerman.net>
2022-06-04 08:25:28 -0400

    fixing the string length identifier on line 7 from !== to <=

--- string-repeater.js
+++ string-repeater.js
@@ -4,7 +4,7 @@ let string = (prompt("Enter a string: "));
 
 let string2 = string
 
-while (string.length !==10){
+while (string.length <= 10){
     string = string + string2
     console.log(string)
     console.log("Your string is " + string.length + " characters.")
